353c8344dbe3fe311c245ffb3d0a8309c2026c54
mageshsudhakar <81739304+mageshsudhakar@users.noreply.github.com>
2023-03-06 05:28:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php
-require 'function.php';
-if(isset($_SESSION["id"])){
-  $id = $_SESSION["id"];
-  $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id"));
-}
-else{
-  header("Location: login.php");
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
